de7a712782311588b246305bfe8e237060500314
vincent-tsugranes <vtsugranes@gmail.com>
2021-02-02 16:08:54 -0500

    allow cors

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import express, {Request, Response} from 'express';
 import * as env from 'env-var';
 import * as pino from 'pino';
 import * as path from 'path';
+import cors from "cors";
 import CrewmemberController from './routes/crewmembercontroller';
 
 const PORT = env.get('PORT').default('9002').asPortNumber();
 
 // const app = express();
 export const app: express.Application = express();
+
+app.use(cors());
 
 // Add kubernetes liveness and readiness probes at
 // /api/health/readiness and /api/health/liveness
